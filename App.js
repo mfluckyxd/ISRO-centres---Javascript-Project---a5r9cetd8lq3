ec9f24f3ee3314a024819e3b167073e66e114c10
Kamal Bijaraniyan <52608133+mfluckyxd@users.noreply.github.com>
2023-06-19 15:01:16 +0530

    first commit

--- App.js
+++ App.js
@@ -1,1 +1,62 @@
+const cityBtn = document.getElementById('city-btn')
+const stateBtn = document.getElementById('state-btn')
+const centreBtn = document.getElementById('centre-btn')
+let inputField = document.getElementById('input');
+let allCentres = [];
 
+function getCentres() {
+    fetch('https://isro.vercel.app/api/centres')
+        .then(res => res.json())
+        .then(data => {
+            allCentres = data.centres
+            displayResults(allCentres)
+        })
+}
+
+
+cityBtn.addEventListener('click', filterCentres);
+stateBtn.addEventListener('click', filterCentres);
+centreBtn.addEventListener('click', filterCentres);
+
+
+function filterCentres(event) {
+    const clickedButton = event.target.innerHTML;
+    let filterKey;
+    if (clickedButton == 'City') {
+        filterKey = 'Place';
+    } else if (clickedButton == 'State') {
+        filterKey = 'State';
+    } else {
+        filterKey = 'name';
+    }
+
+    let userInput = inputField.value;
+    let filteredCentres = allCentres.filter(centre => centre[filterKey].toLowerCase().includes(userInput.toLowerCase()))
+    displayResults(filteredCentres);
+}
+
+
+function displayResults(centresToDisplay) {
+    let resTable = document.getElementById('resultTable');
+    resTable.textContent = '';
+
+    let htmlContent = `
+        <tr>
+            <th>Centre</th>    
+            <th>City</th>    
+            <th>State</th>    
+        </tr>
+        `
+    for (let centre of centresToDisplay) {
+        htmlContent += `
+            <tr>
+                <td>${centre.name}</td>
+                <td>${centre.Place}</td>
+                <td>${centre.State}</td>
+            </tr>
+        `
+    }
+    resTable.innerHTML = htmlContent;
+}
+
+window.onload = getCentres;
